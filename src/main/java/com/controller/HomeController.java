4c7700ae2e668d070ef64f57030e0f3b87b89d55
Krzysztof Zybul <krzysztof.zybul@gmail.com>
2018-08-17 19:12:19 +0200

    [JWC] Clearing button stylization

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import com.storage.DataRepository;
+import com.storage.FileUploader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    DataRepository dataRepository;
+
+    public HomeController(final DataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
     @GetMapping("/")
     public String getIndexPage(Model model){
+        dataRepository.getOpsRepository().clear();
+        dataRepository.getJadRepository().clear();
         return "index";
     }
 }
