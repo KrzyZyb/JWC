2e5e224cb69c019020fa57a663ca2d39fd683518
Krzysztof Zybul <krzysztof.zybul@gmail.com>
2018-09-01 19:01:54 +0200

    [JWC] Full Raport algorithm adjusted

--- src/main/java/com/core/WaypointDataComparator.java
+++ src/main/java/com/core/WaypointDataComparator.java
@@ -11,12 +11,12 @@ import java.util.List;
 
 @Service
 @Slf4j
-public class WaypointDataComparator { //TODO: ADD COMPARSION OF WCM NOT TO REPEAT ITSELF
+public class WaypointDataComparator {
     DataRepository dataRepository;
     List<Waypoint> waypointChanges;
     List<WaypointComparsionModel> waypointChangesFull;
     private Logger logger = LoggerFactory.getLogger("Waypoint data comparator:");
-    private static final String EMPTY_VALUE = "";
+
     public WaypointDataComparator(DataRepository dataRepository) {
         this.dataRepository = dataRepository;
         waypointChanges = new ArrayList<>();
@@ -25,45 +25,50 @@ public class WaypointDataComparator { //TODO: ADD COMPARSION OF WCM NOT TO REPEA
 
     public void compareWaypoints(){
         List<Waypoint> opsWaypoints = dataRepository.getOpsRepository();
-
-        for(Waypoint waypoint: opsWaypoints) {
-            isWaypointInJAD(waypoint);
-            hasCoordinateChanged(waypoint);
-            hasIdChanged(waypoint);
+        for(Waypoint opsWaypoint: opsWaypoints) {
+            WaypointComparsionModel fullRaportRow = createOpsForComparsionModel(opsWaypoint);
+            checkIfOpsIsInJad(opsWaypoint, fullRaportRow);
+            hasCoordinateChanged(opsWaypoint, fullRaportRow);
+            hasIdChanged(opsWaypoint, fullRaportRow);
+            waypointChangesFull.add(fullRaportRow);
         }
     }
 
-     boolean isWaypointInJAD(Waypoint opsWaypoint){
-        if(jadContainsOps(opsWaypoint)) {
-            return true;
-        }else{
+     void checkIfOpsIsInJad(Waypoint opsWaypoint, WaypointComparsionModel fullRaportRow){
+        final String WAYPOINT_NOT_FOUND_STATUS = "Waypoint not found";
+        Waypoint jadWaypoint = getJadForOps(opsWaypoint);
+        if(jadWaypoint==null) {
             opsWaypoint.setStatus("Status: waypoint not found");
             waypointChanges.add(opsWaypoint);
-            logger.info("Data Processor: Waypoint with id = "+opsWaypoint.getWPT_id()+" not found in JAD data");
-            return false;
+            logger.info("Data Processor: Waypoint with id = " + opsWaypoint.getWPT_id() + " not found in JAD data");
+            updateFullRaportRowStatus(WAYPOINT_NOT_FOUND_STATUS, fullRaportRow);
+        }else{
+            addJadForComparsionModel(jadWaypoint, fullRaportRow);
         }
     }
 
-     boolean hasCoordinateChanged(Waypoint opsWaypoint){
+     boolean hasCoordinateChanged(Waypoint opsWaypoint, WaypointComparsionModel fullRaportRow){
+        final String INVALID_COORDINATES_STATUS = "Invalid coordinates";
         List<Waypoint> jadWaypoints = dataRepository.getJadRepository();
         for(Waypoint jadWaypoint:jadWaypoints){
             if(jadWaypoint.getWPT_id().equals(opsWaypoint.getWPT_id())&&!jadWaypoint.getLongxlati().equals(opsWaypoint.getLongxlati())){
                 opsWaypoint.setStatus("Status: invalid coordinates");
                 waypointChanges.add(opsWaypoint);
-                waypointChangesFull.add(createWaypointComparsionModel(opsWaypoint,jadWaypoint));
                 logger.info("Data Processor: Waypoint with id = "+opsWaypoint.getWPT_id()+" has different coordinates");
+                updateFullRaportRowStatus(INVALID_COORDINATES_STATUS, fullRaportRow);
                 return true;
             }
         }
         return false;
     }
 
-     boolean hasIdChanged(Waypoint opsWaypoint) {
+     boolean hasIdChanged(Waypoint opsWaypoint, WaypointComparsionModel fullRaportRow) {
+        final String INVALID_ID_STATUS = "Invalid ID";
         List<Waypoint> jadWaypoints = dataRepository.getJadRepository();
         for (Waypoint jadWaypoint : jadWaypoints) {
             if (!jadWaypoint.getWPT_id().equals(opsWaypoint.getWPT_id()) && jadWaypoint.getLongxlati().equals(opsWaypoint.getLongxlati())) {
                 opsWaypoint.setStatus("Status: invalid ID");
-                waypointChangesFull.add(createWaypointComparsionModel(opsWaypoint,jadWaypoint));
+                updateFullRaportRowStatus(INVALID_ID_STATUS, fullRaportRow);
                 waypointChanges.add(opsWaypoint);
                 logger.info("Data Processor: Waypoint with coordinates = " + opsWaypoint.getLongxlati() + " has changed ID");
                 return true;
@@ -72,54 +77,39 @@ public class WaypointDataComparator { //TODO: ADD COMPARSION OF WCM NOT TO REPEA
         return false;
     }
 
-     boolean jadContainsOps(Waypoint opsWaypoint){
+     Waypoint getJadForOps (Waypoint opsWaypoint){
         List<Waypoint> jadWaypoints = dataRepository.getJadRepository();
         for (Waypoint jadWaypoint : jadWaypoints) {
             if (jadWaypoint.getWPT_id().equals(opsWaypoint.getWPT_id()) || jadWaypoint.getLongxlati().equals(opsWaypoint.getLongxlati())) {
-                waypointChangesFull.add(createWaypointComparsionModel(opsWaypoint,jadWaypoint));
-                return true;
+                return jadWaypoint;
             }
         }
-         waypointChangesFull.add(createWaypointComparsionModel(opsWaypoint,null));
-        return false;
+        return null;
     }
 
-    WaypointComparsionModel createWaypointComparsionModel(Waypoint opsWaypoint, Waypoint jadWaypoint){
-        if(jadWaypoint==null){
-            WaypointComparsionModel waypointComparsionModel = new WaypointComparsionModel();
-            waypointComparsionModel.setCountryOPS(opsWaypoint.getCountry());
-            waypointComparsionModel.setICAOOPS(opsWaypoint.getICAO());
-            waypointComparsionModel.setWPT_idOPS(opsWaypoint.getWPT_id());
-            waypointComparsionModel.setLatitudeOPS(opsWaypoint.getLatitude());
-            waypointComparsionModel.setLongitudeOPS(opsWaypoint.getLongitude());
-            waypointComparsionModel.setLongxlatiOPS(opsWaypoint.getLongxlati());
-            waypointComparsionModel.setStatus("Status: waypoint not found");
+    WaypointComparsionModel createOpsForComparsionModel(Waypoint opsWaypoint){
+        WaypointComparsionModel waypointComparsionModel = new WaypointComparsionModel();
+        waypointComparsionModel.setCountryOPS(opsWaypoint.getCountry());
+        waypointComparsionModel.setICAOOPS(opsWaypoint.getICAO());
+        waypointComparsionModel.setWPT_idOPS(opsWaypoint.getWPT_id());
+        waypointComparsionModel.setLatitudeOPS(opsWaypoint.getLatitude());
+        waypointComparsionModel.setLongitudeOPS(opsWaypoint.getLongitude());
+        waypointComparsionModel.setLongxlatiOPS(opsWaypoint.getLongxlati());
+        waypointComparsionModel.setStatus("");
+        return waypointComparsionModel;
+    }
 
-            waypointComparsionModel.setCountryJAD(EMPTY_VALUE);
-            waypointComparsionModel.setICAOJAD(EMPTY_VALUE);
-            waypointComparsionModel.setWPT_idJAD(EMPTY_VALUE);
-            waypointComparsionModel.setLatitudeJAD(EMPTY_VALUE);
-            waypointComparsionModel.setLongitudeJAD(EMPTY_VALUE);
-            waypointComparsionModel.setLongxlatiJAD(EMPTY_VALUE);
-            return waypointComparsionModel;
-        }else{
-            WaypointComparsionModel waypointComparsionModel = new WaypointComparsionModel();
-            waypointComparsionModel.setCountryOPS(opsWaypoint.getCountry());
-            waypointComparsionModel.setICAOOPS(opsWaypoint.getICAO());
-            waypointComparsionModel.setWPT_idOPS(opsWaypoint.getWPT_id());
-            waypointComparsionModel.setLatitudeOPS(opsWaypoint.getLatitude());
-            waypointComparsionModel.setLongitudeOPS(opsWaypoint.getLongitude());
-            waypointComparsionModel.setLongxlatiOPS(opsWaypoint.getLongxlati());
-            waypointComparsionModel.setStatus(opsWaypoint.getStatus());
+    private void addJadForComparsionModel(Waypoint jadWaypoint, WaypointComparsionModel waypointComparsionModel){
+        waypointComparsionModel.setCountryJAD(jadWaypoint.getCountry());
+        waypointComparsionModel.setICAOJAD(jadWaypoint.getICAO());
+        waypointComparsionModel.setWPT_idJAD(jadWaypoint.getWPT_id());
+        waypointComparsionModel.setLatitudeJAD(jadWaypoint.getLatitude());
+        waypointComparsionModel.setLongitudeJAD(jadWaypoint.getLongitude());
+        waypointComparsionModel.setLongxlatiJAD(jadWaypoint.getLongxlati());
+    }
 
-            waypointComparsionModel.setCountryJAD(jadWaypoint.getCountry());
-            waypointComparsionModel.setICAOJAD(jadWaypoint.getICAO());
-            waypointComparsionModel.setWPT_idJAD(jadWaypoint.getWPT_id());
-            waypointComparsionModel.setLatitudeJAD(jadWaypoint.getLatitude());
-            waypointComparsionModel.setLongitudeJAD(jadWaypoint.getLongitude());
-            waypointComparsionModel.setLongxlatiJAD(jadWaypoint.getLongxlati());
-            return waypointComparsionModel;
-        }
+    public void updateFullRaportRowStatus(String statusUpdate, WaypointComparsionModel fullRaportRow){
+        fullRaportRow.setStatus(fullRaportRow.getStatus().concat(" "+statusUpdate));
     }
 
     public List<Waypoint> getWaypointChanges(){
